775095537fdc6f95f162f319ab31b16bce42d415
bgallamoza <brennan.gallamoza@gmail.com>
2022-03-08 09:37:14 -0500

    refactored plot_vector()

--- stream/stream.py
+++ stream/stream.py
@@ -15,24 +15,20 @@ def plot_vector(x_bounds:"tuple[int, int]", y_bounds:"tuple[int, int]", vector:"
     color: default color of plotted vector"""
     # print(x_bounds, y_bounds) # debugging print statement
 
-    x_origin = None # x coordinate of the vector's origin point
-    y_origin = None # y coordinate of the vector's origin point
-    scale = None # number of data units per arrow length
+    axis_len = None # len of shortest axis
 
-    width = x_bounds[1] - x_bounds[0]
-    height = y_bounds[0] - y_bounds[1]
+    # checks if height < width; the shorter axis is used for subsequent calculations
+    if (y_bounds[0] - y_bounds[1]) < (x_bounds[1] - x_bounds[0]):
+        axis_len = y_bounds[0] - y_bounds[1]
+    else:
+        axis_len = x_bounds[1] - x_bounds[0]
 
     # Plot vector according the the shorter axis. Origin point is situated at
     # the top right of the window, offset by 1/14 of the shorter axis length.
     # Scale adjusted to be 1/25 of the shorter axis length.
-    if height < width:
-        x_origin = x_bounds[1] - height/14
-        y_origin = y_bounds[1] + height/14
-        scale = height/25
-    else:
-        x_origin = x_bounds[1] - width/14
-        y_origin = y_bounds[1] + width/14
-        scale = width/25
+    x_origin = x_bounds[1] - axis_len/14    # x coordinate of the vector's origin point
+    y_origin = y_bounds[1] + axis_len/14    # y coordinate of the vector's origin point
+    scale = axis_len/25 # number of data units per arrow length
 
     # plots vector originating from the designated origin
     plt.quiver(x_origin, y_origin, vector[0], vector[1], color=color, scale=scale)
